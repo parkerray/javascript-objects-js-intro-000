2e02929ced2733a2a95926da08f6c29e39680e3c
Parker Thompson <park.thompson@gmail.com>
2019-10-18 03:41:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {
   traciChapman : "fast car",
   childishGambino : "sober"
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
